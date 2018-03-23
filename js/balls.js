7636afa96c9e34fbc7f1f188fb46110bec9d1cb4
Connor Vanderhook <connor@trueanthem.com>
2018-03-23 12:19:36 -0700

    feat: improve randomizer, look, and feel

--- js/balls.js
+++ js/balls.js
@@ -1,19 +1,19 @@
 var balls=[];
-var NOB = 13;
+var num = 101;
 
 function setup() {
-  createCanvas(600,600);
-  for(var i=0; i<NOB; i++) {
-    balls.push(new Ball());
+  createCanvas(800,600);
+  for(var i=0; i<num; i++) {
+    balls.push(new ball());
   }
 }
 
 function draw() {
   background(0);
-  for(var i=0; i<NOB; i++) {
-    balls[i].move();
+  for(var i=0; i<num; i++) {
+    balls[i].move(i);
     balls[i].disp();
-    for(var j=0; j<NOB; j++) {
+    for(var j=0; j<num; j++) {
       if(i!=j){
         balls[i].connect(balls[j]);
       }
@@ -21,27 +21,34 @@ function draw() {
   }
 }
 
-function Ball() {
-  this.d=20;
-  this.xPos= random(this.d,width-this.d/2);
-  this.yPos= random(this.d,height-this.d/2);
-  this.xPosf=random(0.1,3);
-  this.yPosf=random(0.1,3);
+function ball() {
+  this.d=3;
+  this.xPos=random(this.d,width-this.d/1.5);
+  this.yPos=random(this.d,height-this.d/1.5);
+  this.xPosf=random(0.1,1.25);
+  this.yPosf=random(0.1,1.25);
   this.disp=function() {
     fill(255);
     noStroke();
     ellipse(this.xPos,this.yPos,this.d,this.d);
   }
-  this.move=function() {
-    this.xPos+=this.xPosf;
-    this.yPos+=this.yPosf;
-    if(this.xPos>width-this.d/2 || this.xPos<this.d/2){this.xPosf=this.xPosf*-1;}
+  this.move=function(num) {
+    if (isOdd(num)) {
+      this.xPos+=this.xPosf;
+      this.yPos-=this.yPosf;
+    } else {
+      this.xPos-=this.xPosf;
+      this.yPos+=this.yPosf;
+    }
+    if(this.xPos>width-this.d/2|| this.xPos<this.d/2){this.xPosf=this.xPosf*-1;}
     if(this.yPos>height-this.d/2 || this.yPos<this.d/2){this.yPosf=this.yPosf*-1;}
   }
-  this.connect = function(other) {
-    if (dist(this.xPos,this.yPos,other.xPos,other.yPos)<100) {
-      stroke(255,0,0);
+  this.connect=function(other) {
+    if (dist(this.xPos,this.yPos,other.xPos,other.yPos)<50) {
+      stroke(0,255,0);
       line(this.xPos,this.yPos,other.xPos,other.yPos);
     }
   }
 }
+
+function isOdd(num){return num % 2;}
